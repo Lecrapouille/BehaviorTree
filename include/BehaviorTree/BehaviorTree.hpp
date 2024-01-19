5c212bcfda16755680371e47ce3f71b8fe1cd9f8
Quentin Quadrat <lecrapouille@gmail.com>
2024-01-19 19:39:38 +0100

    Add license banner

--- include/BehaviorTree/BehaviorTree.hpp
+++ include/BehaviorTree/BehaviorTree.hpp
@@ -1,6 +1,37 @@
-// BrainTree - A C++ behavior tree single header library.
-// Copyright 2015-2018 Par Arvidsson. All rights reserved.
-// Licensed under the MIT license (https://github.com/arvidsson/BrainTree/blob/master/LICENSE).
+//*****************************************************************************
+// A C++ behavior tree lib https://github.com/Lecrapouille/BehaviorTree
+//
+// MIT License
+//
+// Copyright (c) 2024 Quentin Quadrat <lecrapouille@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//*****************************************************************************
+//
+// This project is based on this initial project:
+//
+//   BrainTree - A C++ behavior tree single header library.
+//   Copyright 2015-2018 Par Arvidsson. All rights reserved.
+//   Licensed under the MIT license
+//   (https://github.com/arvidsson/BrainTree/blob/master/LICENSE).
+//
+//*****************************************************************************
 
 #pragma once
 
